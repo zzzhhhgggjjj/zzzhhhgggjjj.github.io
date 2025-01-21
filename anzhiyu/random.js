2468038865d2653a994e401ad64469f72035357b
zzzhhhgggjjj <3319545748@qq.com>
2025-01-21 22:17:07 +0800

    Site updated: 2025-01-21 22:16:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["wenzhang/hello-world/","wenzhang/大数据开发项目/"];function toRandomPost(){
+var posts=["wenzhang/hello-world/","wenzhang/大数据开发项目/","wenzhang/这是一篇新的博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
