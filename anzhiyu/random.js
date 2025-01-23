afdb8df14a09396a47aaec95cbd41702e4e2923b
zzzhhhgggjjj <3319545748@qq.com>
2025-01-23 21:43:22 +0800

    Site updated: 2025-01-23 21:43:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["wenzhang/大数据开发项目/","wenzhang/在线教育离线数仓之数据采集/","wenzhang/hello-world/","wenzhang/这是一篇新的博文/"];function toRandomPost(){
+var posts=["wenzhang/Maxwell/","wenzhang/在线教育离线数仓之数据采集/","wenzhang/hello-world/","wenzhang/大数据开发项目/","wenzhang/这是一篇新的博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
