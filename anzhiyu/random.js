3db4efa8ebebdc3114157c515a03f1d4e0935b74
zzzhhhgggjjj <3319545748@qq.com>
2025-01-31 15:37:47 +0800

    Site updated: 2025-01-31 15:37:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["wenzhang/Datax/","wenzhang/Maxwell/","wenzhang/在线教育离线数仓之数据采集/","wenzhang/hello-world/","wenzhang/大数据开发项目/","wenzhang/这是一篇新的博文/"];function toRandomPost(){
+var posts=["wenzhang/Maxwell/","wenzhang/Datax/","wenzhang/hello-world/","wenzhang/在线教育离线数仓之数据采集/","wenzhang/Maxwell启动失败处理方法/","wenzhang/建模/","wenzhang/梳理/","wenzhang/大数据开发项目/","wenzhang/这是一篇新的博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
