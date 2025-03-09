9090343ddeba26e710e0b32a674e825cec6aa591
zzzhhhgggjjj <3319545748@qq.com>
2025-03-09 21:19:11 +0800

    Site updated: 2025-03-09 21:18:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["wenzhang/# 数仓数据治理平台介绍/","wenzhang/Datax/","wenzhang/Maxwell/","wenzhang/Maxwell启动失败处理方法/","wenzhang/flink/","wenzhang/hello-world/","wenzhang/linuxone服务器免费使用/","wenzhang/在线教育离线数仓之数据采集/","wenzhang/大数据优化/","wenzhang/大数据开发项目/","wenzhang/建模/","wenzhang/数据中台/","wenzhang/数据中台技术/","wenzhang/数据治理/","wenzhang/数据治理项目介绍/","wenzhang/梳理/","wenzhang/这是一篇新的博文/"];function toRandomPost(){
+var posts=["wenzhang/# 数仓数据治理平台介绍/","wenzhang/Datax/","wenzhang/flink/","wenzhang/Maxwell启动失败处理方法/","wenzhang/hello-world/","wenzhang/Maxwell/","wenzhang/在线教育离线数仓之数据采集/","wenzhang/大数据优化/","wenzhang/大数据开发项目/","wenzhang/linuxone服务器免费使用/","wenzhang/建模/","wenzhang/数据中台/","wenzhang/数据治理项目介绍/","wenzhang/数据中台技术/","wenzhang/数据治理/","wenzhang/梳理/","wenzhang/这是一篇新的博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
